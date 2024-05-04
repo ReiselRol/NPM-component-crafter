77b36d0e17649d0cffef5f31e75d64a7249b1f12
Rios, Ramon <125033932+ReiselRol@users.noreply.github.com>
2024-05-04 17:22:40 +0200

    Works Perfectly

--- src/component-crafter.js
+++ src/component-crafter.js
@@ -13,7 +13,7 @@ const DEFAULT_CONFIG = {
             Scaffold: [
                 {
                     type: "folder",
-                    name: "#SPECIAL#",
+                    name: "|SPECIAL|",
                     content: [
                         {
                             type: "folder",
@@ -22,13 +22,13 @@ const DEFAULT_CONFIG = {
                         },
                         {
                             type: "file",
-                            name: "#SPECIAL#.css",
+                            name: "|SPECIAL|.css",
                             content: ""
                         },
                         {
                             type: "file",
-                            name: "#SPECIAL#.tsx",
-                            content: "import './#SPECIAL#.css'\n\nfunction #SPECIAL#({}) {\n  return (\n    <>\n\n    </>\n  )\n\n}\nexport default #SPECIAL#"
+                            name: "|SPECIAL|.tsx",
+                            content: "import './|SPECIAL|.css'\n\nfunction |SPECIAL|({}) {\n  return (\n    <>\n\n    </>\n  )\n\n}\nexport default |SPECIAL|"
                         }
                     ]
                 }
@@ -40,13 +40,14 @@ const DEFAULT_CONFIG = {
 function componentCrafterCall() {
 
     const PROJECT_ROOT = process.argv[1].replace('\\node_modules\\component-crafter\\src\\component-crafter.js', '')
-    const COMMAND = process.argv[2].toLowerCase()
-    const COMMAND_PARAMETER = process.argv[3].toLowerCase()
+    const COMMAND = (process.argv[2] != undefined) ? process.argv[2].toLowerCase() : undefined
+    const COMMAND_SPECIAL = (process.argv[3] != undefined) ? process.argv[3].toLowerCase() : undefined
+    const COMMAND_PARAMETER = (process.argv[4] != undefined) ? process.argv[4].toLowerCase()  : undefined
 
     if (COMMAND != undefined) 
     { 
         if (COMMAND.toLowerCase() == "i" || COMMAND.toLowerCase() == "-i" || COMMAND.toLowerCase() == "init" || COMMAND.toLowerCase() == "install") createEnviroment(PROJECT_ROOT)
-        else executeCraftCommand(COMMAND, PROJECT_ROOT, COMMAND_PARAMETER)
+        else executeCraftCommand(COMMAND, PROJECT_ROOT, COMMAND_PARAMETER, COMMAND_SPECIAL)
     }
     else {
         let text = "Welcome to Component Crafter! This terminal aplication is used for "
@@ -107,7 +108,7 @@ function crafterLog(message, tag) {
 
 }
 
-async function executeCraftCommand (command, startingPath, commandParameter) {
+async function executeCraftCommand (command, startingPath, commandParameter, special) {
     if (isConfigCreated(startingPath) == true) {
         const configurationBuffer = await fs.readFile(path.join(startingPath, CONFIG_FILE_NAME))
         const configurationData = JSON.parse(configurationBuffer)
@@ -118,7 +119,11 @@ async function executeCraftCommand (command, startingPath, commandParameter) {
                     if (commandParameter != undefined) {    
                         if (configurationData["commands"][configurationCommandIndex] != undefined) crafterLog(configurationData["commands"][configurationCommandIndex].name + ": " + configurationData["commands"][configurationCommandIndex].description, 'I')
                         else crafterLog("Component Crafter's configuration don't have assigned the description for this command.\nPlease insert some description under the name of the command with the tag 'description'.", 'I')
-                    } else recursiveFileCreator(configurationData["commands"][configurationCommandIndex], startingPath, 0)
+                    } else {
+                        let commandStartingPath = startingPath
+                        if (configurationData["commands"][configurationCommandIndex]["startingPath"] != undefined && configurationData["commands"][configurationCommandIndex]["startingPath"].length > 0) commandStartingPath = path.join(startingPath, ...configurationData["commands"][configurationCommandIndex]["startingPath"])
+                        const treeOfNewElements = await recursiveFileCreator(configurationData["commands"][configurationCommandIndex]["Scaffold"], commandStartingPath, special, 0)
+                    }
                 } else crafterLog("Component Crafter can't find the specified command '" + command + "'.\nPlease run the command yarn crafter help to see all commands are loaded.", 'E')
             } else crafterLog("Component Crafter has no commands yet, please add some commands inside of 'commands'\n array.", 'E')
         } else crafterLog("Component Crafter's configuration apparently is corrupted. It can't find 'commands'\npart. This part will we on this lines of the document:\n\n{\n   'commands': [   <--- this line\n      ...\n\nTry to fix it or delete the configuration and do yarn crafter init to create a brand \nnew configuration.", 'E')
@@ -136,8 +141,29 @@ function searchCommand (arrayToSearch, command) {
     return commandIndex
 }
 
-function recursiveFileCreator (scaffold, startingPath, depth) {
-
+async function recursiveFileCreator (scaffold, startingPath, special, depth = 0, firstElement = true) {
+    let textOfTheCreation = ""
+    for (let i = 0; i < scaffold.length; i++) {
+        const data = scaffold[i]
+        if (data["type"] != undefined) {
+            if (data["name"] != undefined && data["name"].length > 0) {
+                let fileName = data["name"]
+                if (fileName.includes("|SPECIAL|")) fileName = fileName.replace("|SPECIAL|", special)
+                const newPath = path.join(startingPath, fileName)
+                if (data["type"] == "folder") {
+                    await fs.ensureDir(newPath)
+                    textOfTheCreation += await recursiveFileCreator(data.content, newPath, special, depth + 1)
+                }
+                else if (data["type"] == "file") {
+                    let content = ""
+                    if (data["content"] != undefined && data["content"].length > 0) content = data["content"]
+                    while(content.includes("|SPECIAL|")) content = content.replace("|SPECIAL|", special)
+                    fs.writeFile(newPath, content)
+                }
+            }
+        }
+    }
+    return textOfTheCreation
 }
 
 componentCrafterCall()
@@ -172,7 +198,7 @@ async function createComponent(COMPONENT_NAME) {
                 const COMPONENT_TEST_PATH = path.join(COMPONENT_PATH, '__test__')
                 await fs.ensureDir(COMPONENT_PATH)
                 await fs.ensureDir(COMPONENT_TEST_PATH)
-                const COMPONENT_CONTENT = `import './${COMPONENT_NAME}.css'\n\nfunction ${COMPONENT_NAME}({}) {\n  return (\n    <>\n\n    </>\n  )\n\n}\nexport default ${COMPONENT_NAME}
+                const COMPONENT_CONTENT = `import './|{COMPONENT_NAME}.css'\n\nfunction |{COMPONENT_NAME}({}) {\n  return (\n    <>\n\n    </>\n  )\n\n}\nexport default |{COMPONENT_NAME}
                 `
                 await fs.writeFile(path.join(COMPONENT_PATH, COMPONENT_NAME + '.css'), '');
                 await fs.writeFile(path.join(COMPONENT_PATH, COMPONENT_NAME + '.tsx'), COMPONENT_CONTENT);
